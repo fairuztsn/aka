c339f2032ffbf3ed96dcab54b3114e2d916240db
Fae <this.fairuz@gmail.com>
2024-12-19 21:42:58 +0700

    Set slow down * 10

--- src/utils/timer.ts
+++ src/utils/timer.ts
@@ -8,7 +8,7 @@ function slowDownForMilliseconds(ms: number) {
 export default function timer<T extends (...args: any[]) => any>(func: T): (...args: Parameters<T>) => {result: ReturnType<T>, time: number} {
     return function (...args: Parameters<T>): {result: ReturnType<T>, time: number} {
         const startTime = performance.now(); 
-        slowDownForMilliseconds(10);
+        slowDownForMilliseconds(100);
         const result = func(...args);
         const endTime = performance.now();
         const elapsedTime = parseFloat(((endTime - startTime)).toFixed(4)); 
